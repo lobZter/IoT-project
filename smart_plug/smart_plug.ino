edafbab49fb0d9bb170b46cc06167b03d6d071cf
lobZter <lobst3rd@gmail.com>
2015-06-04 20:42:08 +0800

    Add RTC

--- smart_plug/smart_plug.ino
+++ smart_plug/smart_plug.ino
@@ -1,16 +1,56 @@
-unsigned long time = 1432280400;
-const int RELAY_PIN = 13;
+#include "Wire.h"
+
+const int RELAY_PIN = 2;
 const int CURRENT_PIN = A0;
+
+const byte DS1307_I2C_ADDRESS = 0x68; // DS1307 (I2C) address
+const byte NubberOfFields = 7; // DS1307 (I2C) address range
+
+// varible for getting current value
 const unsigned long sampleTime = 100000UL;// sample over 100ms, it is an exact number of cycles for both 50Hz and 60Hz mains
 const unsigned long numSamples = 250UL;// choose the number of samples to divide sampleTime exactly, but low enough for the ADC to keep up
 const unsigned long sampleInterval = sampleTime/numSamples;// the sampling interval, must be longer than then ADC conversion time
 const int adc_zero = 511;// relative digital zero of the arudino input from ACS712 (could make this a variable and auto-adjust it)
 
-void setup()
-{
-  Serial.begin(57600);
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH);
+// varible for RTC
+int y; // year
+byte m, d, w, h, mi, s; // month, day, week, hour, minute, second
+
+byte bcdTodec(byte val){
+    return ((val / 16 * 10) + (val % 16));
+}
+
+byte decToBcd(byte val){
+    return ((val / 10 * 16) + (val % 10));
+}
+
+void setTime(byte y, byte m, byte d, byte w, byte h, byte mi, byte s){
+    Wire.beginTransmission(DS1307_I2C_ADDRESS);
+    Wire.write(0);
+    Wire.write(decToBcd(s));
+    Wire.write(decToBcd(mi));
+    Wire.write(decToBcd(h));
+    Wire.write(decToBcd(w));
+    Wire.write(decToBcd(d));
+    Wire.write(decToBcd(m));
+    Wire.write(decToBcd(y));
+    Wire.endTransmission();
+}
+
+void getTime(){
+    Wire.beginTransmission(DS1307_I2C_ADDRESS);
+    Wire.write(0);
+    Wire.endTransmission();
+
+    Wire.requestFrom(DS1307_I2C_ADDRESS, NubberOfFields);
+
+    s = bcdTodec(Wire.read() & 0x7f);
+    mi = bcdTodec(Wire.read());
+    h = bcdTodec(Wire.read() & 0x7f);
+    w = bcdTodec(Wire.read());
+    d = bcdTodec(Wire.read());
+    m = bcdTodec(Wire.read());
+    y = bcdTodec(Wire.read()) + 2000;
 }
 
 void req4currnet()
@@ -29,14 +69,28 @@ void req4currnet()
       prevMicros += sampleInterval;
     }
   }
+  getTime();
+  String time = String(y) + "/" + String(m) + "/" + String(d) + " " + String(h) + ":" + String(mi) + ":" + String(s);  
   float rms = sqrt((float)currentAcc/(float)numSamples) * (27.03 / 1024.0);
   Serial.println(String(rms)+","+String(time));
 }
 
+
+void setup()
+{
+  Serial.begin(57600);
+  
+  // relay setup
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+  
+  // RTC setup
+  Wire.begin();
+  setTime(15,6,3,3,22,37,0); // 2015/6/3(wed)22:37:00
+}
+
 void loop()
 {
-  time += 1;
-  
   String in = "";
   
   if (Serial.available()) {
